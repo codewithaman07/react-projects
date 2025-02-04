13b395dd9cab64bd1a213bed448334f568486c78
codewithaman07 <amansingh2009000@gmail.com>
2025-02-04 19:34:29 +0530

    first

---
+++ Portfolio-website/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div className='text-3xl'>
+            I am a Navbar
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
